fa2ca1747a1fbf2ae5bc94365ee88a7faace0967
Sneekztr <91494447+Sneekztr@users.noreply.github.com>
2021-09-27 19:26:42 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+let myVariable;
+myVariable = 'bob'
+myVariable;
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
